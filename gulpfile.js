cddf4b7bef39e6028e9578a67a99cd1cd0d8ae5e
Colin <colin.diesh@gmail.com>
2016-06-15 09:34:08 -0500

    Add smoosh

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,25 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var browserify = require('gulp-browserify');
 var babel = require('gulp-babel');
+var smoosh = require('gulp-smoosher');
+var htmlmin = require('gulp-htmlmin');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+ 
+gulp.task('minifycss', (cb) => {
+    return gulp.src('css/master.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('css/'));
+});
+
+gulp.task('inlinecss', ['minifycss'], () => {
+    return gulp.src('_preprocess/head.html')
+        .pipe(smoosh())
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('_includes/'));
+});
+
 
 gulp.task('build', () => {
     gulp.src([
@@ -37,8 +56,8 @@ gulp.task('debug', () => {
     .pipe(gulp.dest('js'));
 });
 
-gulp.task('default', ['build']);
-gulp.task('devmode', ['debug']);
+gulp.task('default', ['build','minifycss','inlinecss']);
+gulp.task('devmode', ['debug','minifycss','inlinecss']);
 
 gulp.task('watch', () => {
     gulp.watch('src/hab.js/hab.js', ['default']);
